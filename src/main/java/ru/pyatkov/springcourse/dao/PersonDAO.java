06b37f71fbc28a34b0f9f0e169d38a9d993c18c2
Sergey Pyatkov <spyatkov@smart-ts.ru>
2023-06-28 17:18:34 +0500

    Урок 41 - добавление Spring Validator

--- src/main/java/ru/pyatkov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/pyatkov/springcourse/dao/PersonDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -29,6 +30,11 @@ public class PersonDAO {
     public Person show(int id) {
         return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",new Object[]{id}, new PersonMapper())
                 .stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[]{email}, new PersonMapper())
+                .stream().findAny();
     }
 
     public void save(Person person) {
